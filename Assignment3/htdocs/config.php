2a9903a989e7108d19ac9d6a88ee6e61c9bf3cf1
Vidur <vidurmenezes@gmail.com>
2018-03-25 23:12:25 -0400

    got login to work with test in db

--- Assignment3/htdocs/config.php
+++ Assignment3/htdocs/config.php
@@ -1,7 +1,12 @@
 <?php
-   define('DB_SERVER', '127.0.0.1:3307');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'root');
-   define('DB_DATABASE', 'assignment3');
+   define('DB_SERVER', 'mathlab.utsc.utoronto.ca');
+   define('DB_USERNAME', 'meneze69');
+   define('DB_PASSWORD', 'meneze69');
+   define('DB_DATABASE', 'cscb20w18_meneze69');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+if (mysqli_connect_errno())
+      {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+      }
+
 ?>
