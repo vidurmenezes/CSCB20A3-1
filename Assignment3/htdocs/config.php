3f19e3889c6a3cf5dcdd41729abee776a96dad03
AlexeiCoreiba <alexei.coreiba@gmail.com>
2018-03-25 22:01:06 -0400

    Added more hopeful code

--- Assignment3/htdocs/config.php
+++ Assignment3/htdocs/config.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER', 'localhost:3307');
+   define('DB_SERVER', '127.0.0.1:3307');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'root');
    define('DB_DATABASE', 'assignment3');
