7c4a94ab65be7ef764079197136b6e741c32ac7e
AlexeiCoreiba <alexei.coreiba@gmail.com>
2018-03-30 02:28:52 -0400

    Made progress on remark requests. Not reads and outputs all remark info

--- Assignment3/htdocs/viewremarks.php
+++ Assignment3/htdocs/viewremarks.php
@@ -1,55 +1,92 @@
 <?php
-   include('session.php');
-   if($_SESSION['students']==true){
-       header("location:index.php");
-   }
+include('session.php');
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="news.css">
+  <link rel="stylesheet" type="text/css" href="viewremarks.css">
   <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script><!--Used fontawesome for icons -->
   <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet"> <!--Used google fonts for some fonts -->
 </head>
+
 <body>
   <?php
-    include('navbar.php');
+  include('navbar.php');
+  // test feedback table
+  // $sql = "SELECT instructorid, question1, question2, question3, question4 FROM feedback";
+  // $result = $db->query($sql);
+  // if ($result->num_rows > 0) {
+  //   while ($row = $result->fetch_assoc()) {
+  //     $print = $row['instructorid'].": ".$row['question1'].", ".$row['question2'].", ".$row['question3'].", ".$row['question4']."<br>";
+  //     echo $print;
+  //   }
+  // }
+  // define variables and set to empty values
+  $item = $studentid = $mark = $fullrequest = "";
+  $info = $request = $newMark = $markErr = array();
+  $sql = "SELECT requestid, remarkitem, remarkreason, studentid FROM remarks WHERE requeststatus=1";
+  $result = $db->query($sql);
+  if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+      $item = $row['remarkitem'];
+      $studentid = $row['studentid'];
+      $sql = "SELECT $item FROM marks WHERE utorid='$studentid'";
+      $mark = $db->query($sql)->fetch_assoc()[$item];
+      $info[] = "StudentID: ".$studentid."<br>Item: ".$item;
+      $request[] = "Mark Received: ".$mark.".<br>Request Reason: ".$row['remarkreason'];
+    }
+  } else {
+    echo "0 results";
+  }
+
+  // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  //   $instructor = $_POST["instructors"];
+  //   if (empty($_POST["answer1"])) {
+  //     $answer1Err = "Answer is required";
+  //   } else {
+  //     $answer1 = test_input($_POST["answer1"]);
+  //     if (isset($answer1[255])) {
+  //       $answer1Err = "Answer cannot exceed 255 characters";
+  //     }
+  //   }
+  // }
+  // function test_input($data) {
+  //   $data = trim($data);
+  //   $data = stripslashes($data);
+  //   $data = htmlspecialchars($data);
+  //   return $data;
+  // }
+
   ?>
+
   <div id="background">
     <div id="CourseTitle">
-    <div class="myBounceDiv">
-      <h1>NEWS</h1>
-      <br>
-      <i class="far fa-newspaper fa-5x"></i>
-    </div>
+      <div class="myBounceDiv">
+        <h1>Remark Requests</h1>
+        <br>
+        <i class="far fa-newspaper fa-5x"></i>
       </div>
     </div>
-  <div class="mainsection">
-    <h4>January 2, 2018</h4>
-    <p>
-      Sitting mistake towards his few country ask. You delighted two rapturous six depending objection happiness something the. Off nay impossible dispatched partiality unaffected. Norland adapted put ham cordial. Ladies talked may shy basket narrow see. Him
-      she distrusts questions sportsmen. Tolerably pretended neglected on my earnestly by. Sex scale sir style truth ought.
-    </p>
   </div>
-  <div class="mainsection">
-    <h4>February 15, 2018</h4>
-    <p>
-      Agreed joy vanity regret met may ladies oppose who. Mile fail as left as hard eyes. Meet made call in mean four year it to. Prospect so branched wondered sensible of up. For gay consisted resolving pronounce sportsman saw discovery not. Northward or household
-      as conveying we earnestly believing. No in up contrasted discretion inhabiting excellence. Entreaties we collecting unpleasant at everything conviction.
-    </p>
-  </div>
-  <div class="mainsection">
-    <h4>March 2, 2018</h4>
-    <p>
-      Sussex result matter any end see. It speedily me addition weddings vicinity in pleasure. Happiness commanded an conveying breakfast in. Regard her say warmly elinor. Him these are visit front end for seven walls. Money eat scale now ask law learn. Side
-      its they just any upon see last. He prepared no shutters perceive do greatest. Ye at unpleasant solicitude in companions interested.
-    </p>
-  </div>
-<?php
-   include('footer.php');
-?>
+
+
+  <?php
+    for ($i = 0; $i < sizeof($info); $i++) {
+      echo "<div class=\"mainsection\">";
+      $fullrequest = $info[$i]."<br>".$request[$i];
+      echo $fullrequest;
+      echo "<br>";
+      echo "</div>";
+
+    }
+  ?>
+
+
+  <?php
+  include('footer.php');
+  ?>
 </body>
 
 </html>
